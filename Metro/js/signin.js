f16872f9f0043a68a72862a06f6e3e414e089609
out12348t <out12348t@gmail.com>
2021-10-30 21:49:46 +0700

    hello world
    sign in: done
    sign up: done
    products.js : đang làm

--- Metro/js/signin.js
+++ Metro/js/signin.js
@@ -1,13 +1,49 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
+import { doc, getFirestore, collection, getDocs, addDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDsbnvT30rajihtSFpl3NTK64Qt3JzOWA",
+  authDomain: "metro-6c5b7.firebaseapp.com",
+  projectId: "metro-6c5b7",
+  storageBucket: "metro-6c5b7.appspot.com",
+  messagingSenderId: "35846079387",
+  appId: "1:35846079387:web:ccdce0733ae1624673173e",
+  measurementId: "G-WZM8824LT0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+async function getUser(db) {
+  const usersCol = collection(db, 'users'); // Lấy thông tin của collection users
+  const userSnapshot = await getDocs(usersCol); // Lấy document của collection users
+  const userList = userSnapshot.docs.map(doc => doc.data()); // 
+  return userList;
+}
+
+// Hàm thêm user vào Firestore (users collection)
+async function addUser(db, user) {
+  try {
+    const docRef = await addDoc(collection(db, "users"), user);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+async function updateUser(db, document, newValue) {
+  await setDoc(doc(db, 'users', document.id), newValue);
+}
+
 const form = document.getElementById("myForm")
 
 const currentUser = JSON.parse(localStorage.getItem('currentUser'))
 
-if(currentUser){
+if (currentUser) {
   window.location.href = './index.html'
 }
 
 
-function displayErrorMessage(title){
+function displayErrorMessage(title) {
   Swal.fire({
     title,
     icon: 'error',
@@ -15,7 +51,7 @@ function displayErrorMessage(title){
   })
 }
 
-function displaySuccessMessage(title){
+function displaySuccessMessage(title) {
   Swal.fire({
     title,
     icon: 'success',
@@ -23,28 +59,28 @@ function displaySuccessMessage(title){
   })
 }
 
-function handleSubmitForm(e){
+async function handleSubmitForm(e) {
   e.preventDefault()
 
   // Lấy thông tin đăng nhập của người dùng từ form input (email và password)
-  const emailInput = form['email'].value 
+  const emailInput = form['email'].value
   const passwordInput = form['password'].value
 
   // Lấy thông tin account có sẵn trong localStorage có key là 'user'
-  let users = JSON.parse(localStorage.getItem("users"))
-
-  if(!users){
+  let users = await getUser(db)
+ 
+  if (!users) {
     users = []
   }
 
   let userIndexFound = users.findIndex(user => user.email === emailInput)
-  
-  if(userIndexFound === -1){
-    displayErrorMessage("User not found!")
-    return 
-  } 
 
-  if(users[userIndexFound].password != passwordInput){
+  if (userIndexFound === -1) {
+    displayErrorMessage("User not found!")
+    return
+  }
+
+  if (users[userIndexFound].password != passwordInput) {
     displayErrorMessage("Password does not match!")
     return
   } else {
@@ -55,10 +91,10 @@ function handleSubmitForm(e){
     }
     localStorage.setItem('currentUser', JSON.stringify(currentUser))
     displaySuccessMessage("Welcome back!")
-    
+
     window.location.href = './index.html'
   }
 
 }
 
-form.onsubmit = handleSubmitForm
+form.onsubmit = await handleSubmitForm
--- Metro/js/signup.js
+++ Metro/js/signup.js
@@ -1,11 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
+import { doc, getFirestore, collection, getDocs, addDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDsbnvT30rajihtSFpl3NTK64Qt3JzOWA",
+  authDomain: "metro-6c5b7.firebaseapp.com",
+  projectId: "metro-6c5b7",
+  storageBucket: "metro-6c5b7.appspot.com",
+  messagingSenderId: "35846079387",
+  appId: "1:35846079387:web:ccdce0733ae1624673173e",
+  measurementId: "G-WZM8824LT0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+async function addUser(db, user) {
+  try {
+    const docRef = await addDoc(collection(db, "users"), user);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+async function getUser(db) {
+  const usersCol = collection(db, 'users'); // Lấy thông tin của collection users
+  const userSnapshot = await getDocs(usersCol); // Lấy document của collection users
+  const userList = userSnapshot.docs.map(doc => doc.data()); // 
+  
+  return userList;
+}
 const currentUser = JSON.parse(localStorage.getItem('currentUser'))
 
-if(currentUser){
+if (currentUser) {
   window.location.href = './index.html'
 }
 
 
-function displayErrorMessage(title){
+function displayErrorMessage(title) {
   Swal.fire({
     title,
     icon: 'error',
@@ -13,7 +44,7 @@ function displayErrorMessage(title){
   })
 }
 
-function displaySuccessMessage(infoObject){
+function displaySuccessMessage(infoObject) {
   Swal.fire({
     title: 'Sign up successfully!',
     icon: 'success',
@@ -29,44 +60,44 @@ function displaySuccessMessage(infoObject){
   })
 }
 
-function checkPassword(password, confirmedPassword){
+function checkPassword(password, confirmedPassword) {
   // TODO: check if password length less than 8, write the condition for the if statement inside ()
-  if(password.length < 8){
+  if (password.length < 8) {
     displayErrorMessage('Password must be at least 8 characters')
-    return 
+    return
   }
 
   // TODO: check if password must contain at least one number, write the condition for the if statement inside ()
-  if(!
-    (password.includes(0) 
-    || password.includes(1) 
-    || password.includes(2) 
-    || password.includes(3) 
-    || password.includes(4) 
-    || password.includes(5) 
-    || password.includes(6)
-    || password.includes(7)
-    || password.includes(8)
-    || password.includes(9)
+  if (!
+    (password.includes(0)
+      || password.includes(1)
+      || password.includes(2)
+      || password.includes(3)
+      || password.includes(4)
+      || password.includes(5)
+      || password.includes(6)
+      || password.includes(7)
+      || password.includes(8)
+      || password.includes(9)
     )
-  ){
+  ) {
     displayErrorMessage('Password must contain at least one number')
-    return 
+    return
   }
 
   // TODO: check if password is the same as confirm password, write the condition for the if statement inside ()
-  if(password != confirmedPassword){
+  if (password != confirmedPassword) {
     displayErrorMessage("Password and confirm password should match")
-    return 
+    return
   }
-  
-  return true 
+
+  return true
 }
 
 // TODO: use getElementById to get form element and assign to variable
 const form = document.getElementById("myForm")
 
-function handleSubmitForm(e){
+async function handleSubmitForm(e) {
   e.preventDefault()
 
   // Get value of an input from the form with formula form[id].value or form[name].value
@@ -81,11 +112,11 @@ function handleSubmitForm(e){
   const password = form['password'].value
 
   // TODO: get confirm password 
-  const confirmedPassword =  form['confirm_password'].value
+  const confirmedPassword = form['confirm_password'].value
 
   // Check if password and confirmedPassword are valid 
-  if(!checkPassword(password,confirmedPassword)){
-    return 
+  if (!checkPassword(password, confirmedPassword)) {
+    return
   }
 
   // TODO: creat userInfo Object with 3 keys and corresponding values: name, email, job
@@ -95,28 +126,29 @@ function handleSubmitForm(e){
     password: password
   }
 
-  let users = JSON.parse(localStorage.getItem('users'))
-
-  if(!users){
+  let users = await getUser(db)
+  
+  if (!users) {
     users = []
   }
-
-  for(user of users){
-    if(user.email == userInfo.email){
+  
+  for (let user of users) {
+    if (user.email == userInfo.email) {
       displayErrorMessage("Email has already been registered!")
-      return 
+      return
     }
   }
 
-  users.push(userInfo)
+  await addUser(db, userInfo)
 
-  localStorage.setItem('users', JSON.stringify(users))
-  
+
+
   localStorage.setItem('currentUser', JSON.stringify(userInfo))
 
   displaySuccessMessage(userInfo)
   window.location.href = './index.html'
+  
 }
 
 // listen to onsubmit event of the form 
-form.onsubmit = handleSubmitForm
+form.onsubmit = await handleSubmitForm
